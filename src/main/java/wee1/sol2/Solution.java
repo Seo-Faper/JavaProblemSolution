cc8fcf30acf1b7106819f70e10dace8a477fdaba
seo-faper <dswhdcks@gmail.com>
2023-01-15 20:18:14 +0900

    New Solution

---
+++ src/main/java/wee1/sol2/Solution.java
@@ -0,0 +1,11 @@
+package wee1.sol2;
+
+public class Solution
+{
+    public int solution(String dirs) {
+        int answer = 0;
+
+        return answer;
+
+    }
+}
---
+++ src/test/java/wee1/sol2/SolutionTest.java
@@ -0,0 +1,13 @@
+package wee1.sol2;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import wee1.sol2.Solution;
+
+public class SolutionTest {
+    @Test
+    public void testSolution(){
+        wee1.sol2.Solution sol = new Solution();
+        Assertions.assertEquals(7,sol.solution("ULURRDLLU"));
+        Assertions.assertEquals(7,sol.solution("LULLLLLLU"));
+    }
+}
