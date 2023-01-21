4d27d2172a5cbd787cb8d6993273d6a6756800a6
seo-faper <dswhdcks@gmail.com>
2023-01-21 10:55:48 +0900

    init setting

---
+++ src/test/java/wee2/sol1/SolutionTest.java
@@ -0,0 +1,12 @@
+package wee2.sol1;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SolutionTest {
+    @Test
+    public void testSolution(){
+        Solution sol = new Solution();
+        Assertions.assertArrayEquals(new int[] {2,1},sol.solution(new int[] {93,30,55}, new int[]{1,30,5}));
+        Assertions.assertArrayEquals(new int[] {2,1},sol.solution(new int[] {95, 90, 99, 99, 80, 99}, new int[]{1, 1, 1, 1, 1, 1}));
+    }
+}
