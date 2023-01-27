f3ec968a484c6455f15650e1905bf97f0837cb63
seo-faper <dswhdcks@gmail.com>
2023-01-28 00:48:49 +0900

    기능 개발 해결

--- src/main/java/wee2/sol1/Solution.java
+++ src/main/java/wee2/sol1/Solution.java
@@ -1,9 +1,42 @@
 package wee2.sol1;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = { };
-        //
+        System.out.println("==========================================");
+        Stack<Integer> L = new Stack<>();
+
+        for(int i = progresses.length-1; i>=0; i--){
+            int daily = (100 - progresses[i]) / speeds[i];
+            if ((100 - progresses[i]) % speeds[i] != 0) daily++;
+            L.push(daily);
+        }
+
+        System.out.println(L.toString());
+        Stack<Integer> sk = new Stack<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        int init = 0;
+        while (!L.isEmpty()){
+            if(sk.isEmpty()) {
+                sk.push(L.pop());
+                init = sk.peek();
+            }else{
+              //  System.out.println("init : "+init+" , L.peek() : "+L.peek());
+                if(init >= L.peek()){
+                    sk.push(L.pop());
+                }else{
+                    ans.add(sk.size());
+                    init = sk.peek();
+                    sk.clear();
+                }
+            }
+         //   System.out.println("sk: "+sk.toString());
+          //   System.out.println("L: "+L.toString());
+        }
+        ans.add(sk.size());
+        int[] answer = ans.stream().mapToInt(i -> i).toArray();
         return answer;
     }
 }
--- src/test/java/wee2/sol1/SolutionTest.java
+++ src/test/java/wee2/sol1/SolutionTest.java
@@ -7,6 +7,7 @@ public class SolutionTest {
     public void testSolution(){
         Solution sol = new Solution();
         Assertions.assertArrayEquals(new int[] {2,1},sol.solution(new int[] {93,30,55}, new int[]{1,30,5}));
-        Assertions.assertArrayEquals(new int[] {2,1},sol.solution(new int[] {95, 90, 99, 99, 80, 99}, new int[]{1, 1, 1, 1, 1, 1}));
+        Assertions.assertArrayEquals(new int[] {1, 3, 2},sol.solution(new int[] {95, 90, 99, 99, 80, 99}, new int[]{1, 1, 1, 1, 1, 1}));
+        Assertions.assertArrayEquals(new int[] {1, 1, 1, 4},sol.solution(new int[] {99, 98, 97, 96, 99, 98, 97}, new int[]{1, 1, 1, 1, 1, 1, 1}));
     }
 }
