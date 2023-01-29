1f47154bf12c0f7e00e6ff28893b7f248f912245
seo-faper <dswhdcks@gmail.com>
2023-01-30 01:57:52 +0900

    다리를 지나는 트럭 해결

--- src/main/java/wee2/sol2/Solution.java
+++ src/main/java/wee2/sol2/Solution.java
@@ -1,29 +1,32 @@
 package wee2.sol2;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
-
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
-        Stack<Integer> wait = new Stack<>();
         Queue<Integer> bridge = new LinkedList<>();
-        ArrayList<Integer> off = new ArrayList<>();
-        for(int i = truck_weights.length-1; i>=0; i--){
-            wait.push(truck_weights[i]);
-        }
+
         for(int i = 0; i<bridge_length; i++) bridge.offer(0);
 
-        while (off.size()!=truck_weights.length){
-            if(sumBridge(bridge) + wait.peek() <= weight){
-                bridge.offer(wait.pop());
-                off.add(bridge.poll());
-                answer++;
+        int i = 0;
+        int sum = 0;
+
+        while (i < truck_weights.length){
+            sum -= bridge.poll(); // 다리의 끝에 있는 요소를 내림 0으로 꽉 차있었다면 무게의 변화가 일어나지 않음
+            answer++;
+            if(sum + truck_weights[i] <= weight) { // 현재 다리 위에 트럭 무게 + 지날려는 트럭 무게가 최대 중량보다 작거나 같을 때
+
+                sum += truck_weights[i]; // 다리 중량에 들어갈 트럭을 추가
+                bridge.offer(truck_weights[i]); // 다리 큐에 트럭을 추가
+                i++;
+            }else{ // 현재 다리 위의 트럭 무게 + 지금 지날려는 트럭 무게가 최대 중량보다 클 때
+                bridge.offer(0);
             }
+
+          //  System.out.println(bridge);
         }
-        return answer;
+        return answer + bridge_length;
     }
     public static int sumBridge(Queue<Integer> t){
         Queue<Integer> q = new LinkedList<>(t);
